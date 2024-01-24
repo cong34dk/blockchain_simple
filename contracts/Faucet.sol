35310d70553e430dacd4f76f13f5f611352a51bd
Cong <cong34dk@gmail.com>
2024-01-24 23:29:48 +0700

    update code

---
+++ contracts/Faucet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet{
+    uint public so_nguyen_duong = 1204012;
+    int public so_nguyen = -1291;
+    address private minter;
+}
